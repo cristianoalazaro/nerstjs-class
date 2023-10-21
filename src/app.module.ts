f2b6e801abb4243efdd7ce44befaf58d65ea6148
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2023-10-21 19:38:42 -0300

    finsh safe inplementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    UserModule, 
+    AuthModule,
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 200,
+    }])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
+    
+  ],
 })
 export class AppModule {}
