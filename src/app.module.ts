83641eb803672bf791816c5e60900ac56b260263
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2023-10-25 07:18:06 -0300

    start send email implementing

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
@@ -13,7 +15,20 @@ import { APP_GUARD } from '@nestjs/core';
     ThrottlerModule.forRoot([{
       ttl: 60000,
       limit: 200,
-    }])
+    }]),
+    MailerModule.forRoot({
+      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [
